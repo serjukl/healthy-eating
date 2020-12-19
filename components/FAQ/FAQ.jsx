049686616cd9d1211c021950d747f2dd76e15d08
serjukl <serjukl98@gmail.com>
2020-12-19 23:13:04 +0200

    FAQ component done

---
+++ components/FAQ/FAQ.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './FAQ.module.sass'
+
+const Faq = ({ item, openHandler, isOpen }) => (
+  <div className={styles.container}>
+    <div
+      className={isOpen ? `${styles.questionContainer} ${styles.questionContainerActive}` : styles.questionContainer}
+      onClick={() => openHandler(isOpen === item.id.toString() ? null : item.id)}
+    >
+      <p>
+        {
+          item.question
+        }
+      </p>
+      <div
+        className={styles.openAnswer}
+      >
+        <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M10 12L20 22L30 12L34 14L20 28L6 14L10 12Z" fill="#545454" />
+        </svg>
+      </div>
+    </div>
+    <div className={styles.answerContainer} style={{ display: isOpen ? 'flex' : 'none' }}>
+      <p>
+        {
+            item.answer
+        }
+      </p>
+    </div>
+  </div>
+)
+
+export default Faq
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,45 @@
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.sass'
+import Faq from '../components/FAQ/FAQ'
 
-export default function Home() {
+const Home = () => {
+  const [questionIsOpen, questionIsOpenHandler] = useState(null)
+  const FAQestions = [
+    {
+      id: 0,
+      question: 'Раціон підійде будь-кому?',
+      answer: 'Збалансоване раціональне харчування полягає у підтриманні необхідної кількості мікроелементів і вітамінів, які організм не може синтезувати, тому вони повинні надходити разом з їжею. Таке харчування іноді прирівнюють до дієт, однак не всяке збалансоване меню сприяє схудненню.'
+    },
+    {
+      id: 1,
+      question: 'Раціон підійде будь-кому? 2',
+      answer: '2 Збалансоване раціональне харчування полягає у підтриманні необхідної кількості мікроелементів і вітамінів, які організм не може синтезувати, тому вони повинні надходити разом з їжею. Таке харчування іноді прирівнюють до дієт, однак не всяке збалансоване меню сприяє схудненню.'
+    }
+  ]
+  useEffect(() => {
+    console.log(questionIsOpen)
+  }, [questionIsOpen])
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div className={styles.FAQContainer}>
+        {
+          FAQestions.map((item, key) => (
+            <Faq
+              item={item}
+              key={key}
+              isOpen={key === questionIsOpen ? questionIsOpen.toString() : null}
+              openHandler={(id) => questionIsOpenHandler(id)}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 }
+
+export default Home
