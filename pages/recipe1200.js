d91f68e044895ade427e00c3d94b4ba7752293f3
serjukl <serjukl98@gmail.com>
2021-01-24 11:32:42 +0200

    finish all

--- pages/recipe1200.js
+++ pages/recipe1200.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/recipes.module.sass'
 
-const recipe1200 = () => (
-  <div className={styles.giveResipe}>
-    <p>Увага збережіть даний файл, адже він доступний у єдиному екземплярі</p>
-    <a href="/1200.pdf" download>Завантажити</a>
-    <p>
-      Подарунковий гайд “Гормони щастя”
-    </p>
-    <a href="/present.pdf" download>Завантажити</a>
-  </div>
-)
+const recipe1200 = () => {
+  const [userChoose, userChooseHandler] = useState(null)
+  useEffect(() => {
+    userChooseHandler(localStorage.getItem('recipe'))
+  }, [])
+
+  const finish = () => {
+    localStorage.setItem('recipe', null)
+    window.location = '/'
+  }
+  return (
+    <div className={styles.giveResipe}>
+
+      {
+        +userChoose
+          ? (
+            <>
+              <p>Увага збережіть даний файл, адже він доступний у єдиному екземплярі</p>
+              <a href={`/${localStorage.getItem('recipe')}.pdf`} download>Завантажити</a>
+              <p>
+                Подарунковий гайд “Гормони щастя”
+              </p>
+              <a href="/present.pdf" download>Завантажити</a>
+              <button type="button" onClick={() => finish()}>Я завантажив всі файли, завершити</button>
+            </>
+          )
+          : <p><a href="/">Повернутись на основну сторінку</a></p>
+      }
+
+    </div>
+  )
+}
 
 export default recipe1200
